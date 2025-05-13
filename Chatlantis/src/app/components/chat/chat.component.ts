c37467f4469b57c3a0cc135cb662e9212604fce4
Néo ELDUAYEN <nelduayen@iutbayonne.univ-pau.fr>
2025-05-13 15:46:19 +0200

    Created the base of the chat

--- Chatlantis/src/app/components/chat/chat.component.ts
+++ Chatlantis/src/app/components/chat/chat.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  inputValue: string = '';
 
+  onArrowClick(): void {
+    if (this.inputValue) {
+      console.log('Arrow clicked with input:', this.inputValue);
+    }
+  }
 }
