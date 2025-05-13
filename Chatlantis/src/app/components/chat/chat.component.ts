36b4ea8dd5a62fc368b8bae8dc37737760b703a0
Néo ELDUAYEN <nelduayen@iutbayonne.univ-pau.fr>
2025-05-13 16:03:49 +0200

    Made the responsive on the sidebar

--- Chatlantis/src/app/components/chat/chat.component.ts
+++ Chatlantis/src/app/components/chat/chat.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [SidebarComponent, FormsModule],
+  imports: [SidebarComponent, FormsModule, CommonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
   inputValue: string = '';
+  isSidebarOpen: boolean = false;
 
   onArrowClick(): void {
     if (this.inputValue) {
       console.log('Arrow clicked with input:', this.inputValue);
     }
+  }
+
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 }
