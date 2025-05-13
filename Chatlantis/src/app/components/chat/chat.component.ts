7b4413364a76ca7bb9e00e184a26633ab636f20d
Néo ELDUAYEN <nelduayen@iutbayonne.univ-pau.fr>
2025-05-13 14:50:02 +0200

    Added back the json files and created the sidebar

--- Chatlantis/src/app/components/chat/chat.component.ts
+++ Chatlantis/src/app/components/chat/chat.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
